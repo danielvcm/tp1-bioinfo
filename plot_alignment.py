b3b17d3572b12a24310e5f664135bba134922aba
valeria souza <souza.pereira.valeria@gmail.com>
2021-02-04 11:30:33 -0300

    alignemnt plot not opening automatically

---
+++ plot_alignment.py
@@ -0,0 +1,36 @@
+import six.moves.urllib.request as urlreq
+from six import PY3
+import webbrowser
+import dash
+import dash_bio as dashbio
+import dash_html_components as html
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+data = open("input_example.fasta").read()
+
+app.layout = html.Div([
+    dashbio.AlignmentChart(
+        id='alignment-viewer',
+        data=data
+    ),
+    html.Div(id='alignment-viewer-output')
+])
+
+
+@app.callback(
+    dash.dependencies.Output('alignment-viewer-output', 'children'),
+    [dash.dependencies.Input('my-alignment-viewer', 'eventDatum')]
+)
+def update_output(value):
+    if value is None:
+        return 'No data.'
+    return str(value)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+    # webbrowser.open("http://127.0.0.1:8050/", new=1)
